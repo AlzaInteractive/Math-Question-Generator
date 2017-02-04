de2151e51bd240e608724724c9ec55c54e78f51b
ewien <ewin.sutriandi@gmail.com>
2017-02-04 08:36:39 +0800

    find from ratio
    (should add one more layer of abstraction, it's getting messy, gonna do
    it later for the 3rd app)

--- QuestionGenerator/src/com/alza/quiz/qfactory/fraction/RatioQuestionFactory.java
+++ QuestionGenerator/src/com/alza/quiz/qfactory/fraction/RatioQuestionFactory.java
@@ -35,20 +35,23 @@ public class RatioQuestionFactory implements IQuestionFactory{
 		List<Quiz> ql = new ArrayList<Quiz>();
 		for (int i=0;i<6;i++){
 			MultipleChoiceQuiz q = new MultipleChoiceQuiz();
-			if (i % 3 == 0){
+			if (i % 3 == 1){
 				q = generateTypeC();
+				q.setDifficultyLevel(QuizLevel.SEDANG);
 				q.setLessonSubcategory("Bentuk persen, mencari jumlah jika persentase dan total diketahui");
-			} else if (i % 3 == 1){
+			} else if (i % 3 == 2){
+				q = generateTypeB();
+				q.setDifficultyLevel(QuizLevel.SEDANG);
+				q.setLessonSubcategory("Perbandingan, mencari jumlah jika nilai perbandingan dan total diketahui");
+			} else {
 				q = generateTypeA();
+				q.setDifficultyLevel(QuizLevel.MUDAH);
 				q.setLessonSubcategory("Mendeskripsikan perbandingan");
 			}
-			
 			q.setLessonClassifier("Matematika SD");
 			q.setLessonCategory("Pecahan");
 			q.setLessonGrade(5);
 			ql.add(q);
-			
-			
 		}
 		
 		return ql;
@@ -70,9 +73,35 @@ public class RatioQuestionFactory implements IQuestionFactory{
 		q.setQuestion(sce);
 		q.setCorrectAnswer(String.valueOf(partOfTotal2));
 		q.setChoices(total,partOfTotal,partOfTotal2,partOfTotal*2,partOfTotal2*2);
-		q.setDifficultyLevel(QuizLevel.SEDANG);
+		
 		return q;
 	}
+	
+	private MultipleChoiceQuiz generateTypeB() {
+		String sce;
+		MultipleChoiceQuiz q = new MultipleChoiceQuiz();
+		TYPE t = TYPE.findFromRatio;
+		int rat1,rat2,tot,tot1,tot2,multip,min,max;
+		min = 2; max = 13;
+		do {
+			rat1 = CommonFunctionAndValues.getRandomInt(min, max);
+			rat2 = CommonFunctionAndValues.getRandomInt(min, max);
+		} while (MathUtils.findGCDDjikstra(rat1, rat2)>1);
+		multip = CommonFunctionAndValues.getRandomInt(min, max);
+		tot1 = rat1 * multip;
+		tot2 = rat2 * multip;
+		tot = tot1+tot2;
+		sce = getRandomScenario(t);
+		sce = sce.replace("#total?", String.valueOf(tot));
+		sce = sce.replace("#rat1?", String.valueOf(rat1));
+		sce = sce.replace("#rat2?", String.valueOf(rat1));
+		sce = CommonFunctionAndValues.buildScenario(sce);
+		q.setQuestion(sce);
+		q.setCorrectAnswer(String.valueOf(tot1));
+		q.setChoices(tot1,tot,rat1,tot2,multip);
+		return q;
+	}
+	
 	private MultipleChoiceQuiz generateTypeA() {
 		MultipleChoiceQuiz q = new MultipleChoiceQuiz();
 		TYPE t = TYPE.describeRatio;
@@ -84,7 +113,7 @@ public class RatioQuestionFactory implements IQuestionFactory{
 			b = CommonFunctionAndValues.getRandomInt(min, max);
 			c = CommonFunctionAndValues.getRandomInt(min, max);
 			gcd = MathUtils.findGCD(a,b,c);
-		} while (gcd==1);
+		} while (gcd==1 || a==b || b==c || a==c);
 		sce = sce.replace("#part1?", String.valueOf(a));
 		sce = sce.replace("#part2?", String.valueOf(b));
 		sce = sce.replace("#part3?", String.valueOf(c));
@@ -93,9 +122,9 @@ public class RatioQuestionFactory implements IQuestionFactory{
 		q.setCorrectAnswer(a / gcd+":"+b/gcd+":"+c/gcd);
 		q.setChoices(a / gcd+":"+b/gcd+":"+c/gcd,
 				b / gcd+":"+a/gcd+":"+c/gcd,
-				a+b / gcd+":"+b+c/gcd+":"+c+a/gcd,
+				(a+b) / gcd+":"+(b+c)/gcd+":"+(c+a)/gcd,
 				c / gcd+":"+a/gcd+":"+b/gcd);
-		q.setDifficultyLevel(QuizLevel.MUDAH);
+		
 		return q;
 	}
 	
@@ -107,34 +136,34 @@ public class RatioQuestionFactory implements IQuestionFactory{
 				+ "Ia memiliki #total? ekor ayam. #pct1? ayam miliknya adalah ayam pedaging, "
 				+ "sementara sisanya adalah ayam petelur. "
 				+ "Berapakah jumlah ayam petelur yang ia miliki?");
-		sceFindFromPct.add("Luas lahan pekarangan rumah #orang1? adalah #total? meter persegi."
+		sceFindFromPct.add("Luas lahan pekarangan rumah #orang1? adalah #total? meter persegi. "
 				+ "#pct1? digunakan untuk bercock tanam, "
 				+ "sisanya dibiarkan kosong untuk tempat bermain anaknya. "
 				+ "Berapakah meter persegi luas lahan bermain yang tersedia untuk anak #orang1?");
 		sceFindFromPct.add("Anak laki-laki di sekolah #orang1? berjumlah #total? anak "
-				+ "#pct1? gemar bermain bola, sisanya gemar bermain basket"
+				+ "#pct1? gemar bermain bola, sisanya gemar bermain basket "
 				+ "Berapakah jumlah anak yang gemar bermain basket?");
 		sceFindFromRatio.add("Jumlah anak yang suka wortel dibandingkan dengan yang suka"
 				+ " brokoli adalah #rat1? berbading #rat2?. Jika jumlah anak adalah #total? "
 				+ " berapakah jumlah anak yang suka brokoli?");
 		sceFindFromRatio.add("#orang1 memiliki #total? ekor kelinci. "
 				+ "Jumlah kelinci berwarna putih berbanding warna lainnya "
-				+ "adalah #rat1? berbanding rat2? "
+				+ "adalah #rat1? berbanding #rat2? "
 				+ "Berapakah jumlah kelinci putih #orang1??");
-		sceFindFromRatio.add("Berapakah jumlah guru perempuan di sekolah #orang1 "
+		sceFindFromRatio.add("Berapakah jumlah guru perempuan di sekolah #orang1? "
 				+ "jika perbandingan "
 				+ " antara jumlah guru laki-laki dengan perempuan adalah #rat1? berbanding #rat2?"
 				+ " dan jumlah semua guru adalah #total?.");
-		sceFindRatio.add("#part1 siswa mengikuti ekstrakurikuler pramuka "
-				+ "#part2 siswa mengikuti ekstrakurikuler PMR "
-				+ "sementara #part3 siswa mengikuti ekstrakurikuler atletik. "
+		sceFindRatio.add("#part1? siswa mengikuti ekstrakurikuler Pramuka "
+				+ "#part2? siswa mengikuti ekstrakurikuler PMR "
+				+ "sementara #part3? siswa mengikuti ekstrakurikuler atletik. "
 				+ "Perbandingan antara siswa yang mengikuti pramuka, PMR dan atletik adalah?");
 		sceFindRatio.add("Jumlah siswa yang menyukai olahraga bulutangkis adalah #part1?, "
 				+ "sementara sepakbola adalah #part2?, dan voli adalah #part3?. "
-				+ "Perbandingan jumlah siswa penggemar bulutangkis, sepakbola dan voli"
+				+ "Perbandingan jumlah siswa penggemar bulutangkis, sepakbola dan voli "
 				+ "dapat dinyatakan dengan nilai perbandingan?");
 		sceFindRatio.add("Berapakah perbandingan jumlah ikan nila, ikan gurame dan ikan mas "
-				+ "pada kolam #orang1 jika jumlah ikan tersebut masing-masing "
+				+ "pada kolam #orang1? jika jumlah ikan tersebut masing-masing "
 				+ "#part1?, #part2? dan #part3??");
 		
 		int rndFindFromPct = CommonFunctionAndValues.getRandomInt(0, sceFindFromPct.size());
