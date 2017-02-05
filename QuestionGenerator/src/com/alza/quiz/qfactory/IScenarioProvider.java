b32a41245548eaa1ed8f08c0d3eb672c44953de4
ewien <ewin.sutriandi@gmail.com>
2017-02-05 20:50:13 +0800

    +new abstraction layer for scenario, will implements this layer on next topic

---
+++ QuestionGenerator/src/com/alza/quiz/qfactory/IScenarioProvider.java
@@ -0,0 +1,7 @@
+package com.alza.quiz.qfactory;
+
+import java.util.List;
+
+public interface IScenarioProvider {
+	public List<Scenario> getScenarios();
+}
---
+++ QuestionGenerator/src/com/alza/quiz/qfactory/Scenario.java
@@ -0,0 +1,13 @@
+package com.alza.quiz.qfactory;
+
+import com.alza.quiz.model.Quiz;
+
+public class Scenario {
+	private String scenarioDesc;
+	private IQuestionFactory factory;
+	
+	private Quiz getQuiz(){
+		return null;
+	}
+
+}
