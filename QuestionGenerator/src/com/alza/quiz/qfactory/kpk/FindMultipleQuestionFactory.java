098db65f127f112c6763d5e7ef3a2c701b76d0a7
ewien <ewin.sutriandi@gmail.com>
2017-03-15 08:40:51 +0800

    +Find Multiple Question Factory

---
+++ QuestionGenerator/src/com/alza/quiz/qfactory/kpk/FindMultipleQuestionFactory.java
@@ -0,0 +1,58 @@
+package com.alza.quiz.qfactory.kpk;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import com.alza.quiz.model.MultipleChoiceQuiz;
+import com.alza.quiz.model.Quiz;
+import com.alza.quiz.model.QuizLevel;
+import com.alza.quiz.qfactory.IQuestionFactory;
+
+public class FindMultipleQuestionFactory implements IQuestionFactory{
+
+	@Override
+	public Quiz generateQuiz() {
+		List<Quiz> quizList = generateQuizList();
+		int rnd = new Random().nextInt(quizList.size()); 
+		return quizList.get(rnd);
+	}
+
+	@Override
+	public Quiz generateQuiz(QuizLevel quizLevel) {
+		return generateQuiz();
+	}
+
+	@Override
+	public List<Quiz> generateQuizList() {
+		int minBase = 5;
+		int maxBase = 16;
+		List<Quiz> lq = new ArrayList<Quiz>();
+		for (int i=0;i<2;i++){
+			int base;
+			do {
+				base = ThreadLocalRandom.current().nextInt(minBase, maxBase);
+			} while 
+				((i % 2 == 0 && base % 2 ==0)||
+						(i % 2 > 0 && base % 2 >0));
+			MultipleChoiceQuiz q = new MultipleChoiceQuiz();
+			q.setQuestion("Yang merupakan kelipatan dari "+base+" adalah?");
+			q.setCorrectAnswer(base*2+","+base*3+","+base *4);
+			q.addChoice(base*2+","+base*3+","+base *4);
+			q.addChoice((base+1)+","+(base+2)+","+(base+3));
+			q.addChoice((base+2)+","+(base+3)+","+(base+4));
+			q.addChoice(((base+1)*2)+","+((base+1)*3)+","+((base+1) *4));
+			q.addChoice(((base-1)*2)+","+((base-1)*3)+","+((base-1) *4));
+			q.setDifficultyLevel(QuizLevel.MUDAH);
+			q.setLessonSubcategory("Kelipatan bilangan");
+			q.setLessonClassifier("Matematika SD");
+			q.setLessonGrade(4);
+			q.setSubCategoryOrder(0);
+			q.setLessonCategory("Bilangan Romawi");
+			lq.add(q);
+		}
+		return lq;
+	}
+	
+}
