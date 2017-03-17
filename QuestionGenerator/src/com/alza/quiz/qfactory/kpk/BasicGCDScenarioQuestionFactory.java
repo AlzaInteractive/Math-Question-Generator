15a1f94be3d4557375a30a8c7dbb021b8d982c52
ewien <ewin.sutriandi@gmail.com>
2017-03-17 08:01:03 +0800

    xxxxx

---
+++ QuestionGenerator/src/com/alza/quiz/qfactory/kpk/BasicGCDScenarioQuestionFactory.java
@@ -0,0 +1,29 @@
+package com.alza.quiz.qfactory.kpk;
+
+import java.util.List;
+
+import com.alza.quiz.model.Quiz;
+import com.alza.quiz.model.QuizLevel;
+import com.alza.quiz.qfactory.IQuestionFactory;
+
+public class BasicGCDScenarioQuestionFactory implements IQuestionFactory{
+
+	@Override
+	public Quiz generateQuiz() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Quiz generateQuiz(QuizLevel quizLevel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Quiz> generateQuizList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
