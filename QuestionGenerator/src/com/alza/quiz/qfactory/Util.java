d9281977c2e642470f290d302af2d266c3b16f93
ewien <ewin.sutriandi@gmail.com>
2017-04-22 19:19:55 +0800

    Division of two ints

--- QuestionGenerator/src/com/alza/quiz/qfactory/Util.java
+++ QuestionGenerator/src/com/alza/quiz/qfactory/Util.java
@@ -7,6 +7,7 @@ import java.util.Locale;
 import com.alza.quiz.qfactory.fraction.*;
 import com.alza.quiz.qfactory.integer.AdditionOfThreeIntegers;
 import com.alza.quiz.qfactory.integer.AdditionOfTwoIntegers;
+import com.alza.quiz.qfactory.integer.DivisionOfTwoIntegers;
 import com.alza.quiz.qfactory.integer.MultiplicationOfTwoIntegers;
 import com.alza.quiz.qfactory.integer.SubtractionOfTwoIntegers;
 import com.alza.quiz.qfactory.kpk.BasicGCDQuestionFactory;
@@ -62,6 +63,7 @@ public class Util {
 		lqf.add(new SubtractionOfTwoIntegers(loc));
 		lqf.add(new AdditionOfThreeIntegers(loc));
 		lqf.add(new MultiplicationOfTwoIntegers(loc));
+		lqf.add(new DivisionOfTwoIntegers(loc));
 		return lqf;
 	}
 	public static List<IQuestionFactory> getAllIntegerQuestionFactory(){
---
+++ QuestionGenerator/src/com/alza/quiz/qfactory/integer/DivisionOfTwoIntegers.java
@@ -0,0 +1,87 @@
+package com.alza.quiz.qfactory.integer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+import java.util.ResourceBundle;
+import java.util.concurrent.ThreadLocalRandom;
+
+import com.alza.quiz.model.Quiz;
+import com.alza.quiz.model.QuizLevel;
+import com.alza.quiz.model.SimpleQuiz;
+import com.alza.quiz.qfactory.IQuestionFactory;
+
+public class DivisionOfTwoIntegers implements IQuestionFactory{
+	Locale loc;
+	ResourceBundle bundle;
+	public DivisionOfTwoIntegers(Locale loc){
+		this.loc = loc;
+		initStringFromLocale();
+	}
+	public DivisionOfTwoIntegers(){
+		this.loc = new Locale("in", "ID");
+		initStringFromLocale();
+	}
+	private void initStringFromLocale(){
+		bundle = ResourceBundle.getBundle("lang.langbundle", loc);
+	}
+	int numOfQuestion = 4;
+	int[][] bounds = {
+			{5,10},{10,15},{15,25},{25,50}
+	};
+	@Override
+	public Quiz generateQuiz() {
+		List<Quiz> quizList = generateQuizList();
+		int rnd = new Random().nextInt(quizList.size()); 
+		return quizList.get(rnd);
+	}
+
+	@Override
+	public Quiz generateQuiz(QuizLevel quizLevel) {
+		return generateQuiz();
+	}
+
+	@Override
+	public List<Quiz> generateQuizList() {
+		List<Quiz> lq = new ArrayList<Quiz>();
+		for (int i= 0;i<numOfQuestion;i++){
+			int idx;
+			if (i<bounds.length){
+				idx = i;
+			} else {
+				idx = i % bounds.length; 
+			}
+			//System.out.println("index "+idx);
+			int a=0,b=0;
+			do {
+				a = ThreadLocalRandom.current().nextInt(bounds[idx][0], 
+						bounds[idx][1]);
+				b = ThreadLocalRandom.current().nextInt(bounds[idx][0], 
+						bounds[idx][1]);
+			} while (a==b);
+			SimpleQuiz q = new SimpleQuiz();
+			int rslt = a*b;
+			
+			q.setQuestion(rslt+" : "+a);
+			q.setCorrectAnswer(String.valueOf(b));
+			q.setDifficultyLevel(QuizLevel.MUDAH);
+			q.setLessonSubcategory(bundle.getString("integer.divtwonum"));
+			q.setLessonClassifier(bundle.getString("mathelementary"));
+			q.setLessonGrade(4);
+			q.setSubCategoryOrder(4);
+			q.setLocalGeneratorOrder(idx);
+			q.setLessonCategory(bundle.getString("integer"));
+			lq.add(q);
+		}
+		
+		return lq;
+	}
+
+	@Override
+	public List<Quiz> generateQuizList(int numOfQuestion) {
+		this.numOfQuestion = numOfQuestion;
+		return generateQuizList();
+	}
+
+}
