acb2b816d389e5b2b5c883099f06dc76496140ae
ewien <ewin.sutriandi@gmail.com>
2017-08-01 11:27:18 +0800

    add 3D model interface

---
+++ QuestionGenerator/src/com/alza/quiz/qfactory/geom/Shapes3D.java
@@ -0,0 +1,16 @@
+package com.alza.quiz.qfactory.geom;
+
+import java.util.List;
+
+public interface Shapes3D {
+	public double getSurfaceArea();
+	public double getVolume();
+	public List<Shapes2D> getFaces();
+	public int getVerticesCount();
+	public double get2DOccupiedLength(double projRatio, double projAngle);
+	public double get2DOccupiedHeight(double projRatio, double projAngle);
+	public String getName();
+	public Shapes3D createExample();
+	public List<Path> getPaths(int canvasWidth, int canvasHeight, int margin, double projRatio, double projAngle);
+	public List<Path> getPaths(int canvasWidth, int canvasHeight);
+}
