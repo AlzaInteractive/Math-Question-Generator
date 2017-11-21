c934943b2d71c908c152dc349f8a250e62874e0a
ewien <ewin.sutriandi@gmail.com>
2017-11-21 08:46:11 +0800

    add method, calculate mid point of a line

--- QuestionGenerator/src/com/alza/quiz/model/geom/Point2D.java
+++ QuestionGenerator/src/com/alza/quiz/model/geom/Point2D.java
@@ -13,6 +13,12 @@ public class Point2D {
 		return new Point2D(x+xDistance, y+yDistance);
 	}
 	
+	public static Point2D getMidPoint(Point2D p1, Point2D p2){
+		double x = p1.x + (p2.x-p1.x)/2;
+		double y = p1.y + (p2.y-p1.y)/2;
+		return new Point2D(x, y);
+	}
+	
 	@Override
 	public String toString() {
 		return "("+(int)x+","+(int)y+")";
