98c1ab4b8244970f6382b8048c3636f3b82224c6
ewin.sutriandi <ewin.sutriandi@gmail.com>
2017-12-11 23:10:21 +0800

    add labels

--- QuestionGenerator/src/com/alza/quiz/model/geom/Triangle.java
+++ QuestionGenerator/src/com/alza/quiz/model/geom/Triangle.java
@@ -6,6 +6,10 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Triangle implements Shapes2D{
 	private double baseLine,height,shear;
+	private boolean showHeightLine=true;
+	private boolean showVerticeLabel=true;
+	private boolean showHeightLength=true;
+	private boolean showBaselineLength=true;
 	public static final int SCALENE = 1;
 	public static final int ISCOSCELES = 2;
 	public static final int EQUILATERAL = 3;
@@ -48,6 +52,30 @@ public class Triangle implements Shapes2D{
 	}
 	public void setShear(double shear) {
 		this.shear = shear;
+	}
+	public boolean isShowHeightLine() {
+		return showHeightLine;
+	}
+	public void setShowHeightLine(boolean showHeightLine) {
+		this.showHeightLine = showHeightLine;
+	}
+	public boolean isShowVerticesLabel() {
+		return showVerticeLabel;
+	}
+	public void setShowVerticesLabel(boolean showVerticesLabel) {
+		this.showVerticeLabel = showVerticesLabel;
+	}
+	public boolean isShowHeightLength() {
+		return showHeightLength;
+	}
+	public void setShowHeightLength(boolean showHeightLength) {
+		this.showHeightLength = showHeightLength;
+	}
+	public boolean isShowBaselineLength() {
+		return showBaselineLength;
+	}
+	public void setShowBaselineLength(boolean showBaselineLength) {
+		this.showBaselineLength = showBaselineLength;
 	}
 	@Override
 	public double getArea() {
@@ -137,6 +165,22 @@ public class Triangle implements Shapes2D{
 		l.add(Path.createLinePath(getVertices().get(0), getVertices().get(1)));
 		l.add(Path.createLinePath(getVertices().get(1), getVertices().get(2)));
 		l.add(Path.createLinePath(getVertices().get(2), getVertices().get(0)));
+		
+		if (showVerticeLabel) {
+			l.add(Path.createTextPath(String.valueOf("A"), getVertices().get(0), Path.SHIFT_UP,Path.SHIFT_LEFT));
+			l.add(Path.createTextPath(String.valueOf("B"), getVertices().get(1), Path.SHIFT_DOWN,Path.SHIFT_RIGHT));
+			l.add(Path.createTextPath(String.valueOf("C"), getVertices().get(2), Path.SHIFT_DOWN,Path.SHIFT_LEFT));
+		}
+		if (showHeightLine) {
+			l.add(Path.createLinePathDashed(getVertices().get(0), getVertices().get(2).move(shear, 0)));
+		}
+		if (showHeightLength) {
+			l.add(Path.createTextPath(Geom.formatMeasurement(height), getVertices().get(0).move(0, height/2), Path.SHIFT_NONE,Path.SHIFT_LEFT));
+		}
+		if (showBaselineLength) {
+			l.add(Path.createTextPath(Geom.formatMeasurement(baseLine), getVertices().get(2).move(baseLine/2, 0), Path.SHIFT_DOWN,Path.SHIFT_NONE));
+		}
+		
 		return l;
 	}
 
