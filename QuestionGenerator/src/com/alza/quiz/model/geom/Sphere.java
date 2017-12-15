37565d049f0b0ce4a75b522e61fc6beb7690b821
Ewin Sutriandi <ewin.sutriandi@gmail.com>
2017-12-16 07:18:12 +0800

    sphere

--- QuestionGenerator/src/com/alza/quiz/model/geom/Sphere.java
+++ QuestionGenerator/src/com/alza/quiz/model/geom/Sphere.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Sphere implements Shapes3D{
 	private double radius;
+	private boolean showRadiusValue=true;
 	public Sphere() {
 		
 	}
@@ -31,6 +32,12 @@ public class Sphere implements Shapes3D{
 		return getSurfaceArea() * radius /3;
 	}
 
+	public boolean isShowRadiusValue() {
+		return showRadiusValue;
+	}
+	public void setShowRadiusValue(boolean showRadiusValue) {
+		this.showRadiusValue = showRadiusValue;
+	}
 	@Override
 	public List<Shapes2D> getFaces() {
 		return null;
@@ -78,6 +85,12 @@ public class Sphere implements Shapes3D{
 				radius/2, 0, -180)); // oval projection of horizontal cut, front side
 		l.add(Path.createArcPathDashed(center, radius, 
 				radius/2, 0, 180)); // oval projection of horizontal cut, backside
+		
+		if (showRadiusValue) {
+			l.add(Path.createTextPath(Geom.formatMeasurement(radius), 
+					Point3D.getMidPoint(getVertices().get(1), center)
+					, Path.SHIFT_UP,Path.SHIFT_NONE));
+		}
 		
 		return l;
 	}
