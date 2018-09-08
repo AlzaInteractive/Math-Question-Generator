ee6ec502e669f167562824f0d4be13f9adbcdee3
Ewin Sutriandi <ewin.sutriandi@gmail.com>
2018-09-08 14:21:11 +0800

    fix import

--- QuestionGenerator/src/com/alza/quiz/qfactory/lcmgcd/LCMWhichDateRWPQuestionFactory.java
+++ QuestionGenerator/src/com/alza/quiz/qfactory/lcmgcd/LCMWhichDateRWPQuestionFactory.java
@@ -14,7 +14,6 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
-import java.util.Random;
 import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
