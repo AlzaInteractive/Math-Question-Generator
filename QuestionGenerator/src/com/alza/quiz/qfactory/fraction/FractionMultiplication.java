d13454a4149b5cac764977d5f0b64d234bbcbc8e
Ewin Sutriandi <ewin.sutriandi@gmail.com>
2020-08-20 16:08:28 +0800

    simplify question

--- QuestionGenerator/src/com/alza/quiz/qfactory/fraction/FractionMultiplication.java
+++ QuestionGenerator/src/com/alza/quiz/qfactory/fraction/FractionMultiplication.java
@@ -48,7 +48,7 @@ public class FractionMultiplication implements IQuestionFactory{
 		List<Quiz> quizList= new ArrayList<Quiz>();
 		for (int i=0; i<numq; i++){
 			MultipleChoiceQuiz q = null;
-			q = generateTypeA(i);
+			q = generateQuestion(i);
 			q.setDifficultyLevel(QuizLevel.MUDAH);
 			q.setLessonSubcategory(bundle.getString("fraction.multiplication"));
 			q.setLessonClassifier(bundle.getString("mathelementary"));
@@ -61,27 +61,28 @@ public class FractionMultiplication implements IQuestionFactory{
 		return quizList;
 	}
 
-	private MultipleChoiceQuiz generateTypeA(int i) {
+	private MultipleChoiceQuiz generateQuestion(int i) {
 		MultipleChoiceQuiz q = new MultipleChoiceQuiz();
-		int denom = CommonFunctionAndValues.getRandomInt(11, 41);
+		int denom;
 		int a1,a2;
 		int denomLeft,denomRight;
 		int gcdL,gcdR;
 		Fraction f1,f2;
 		if (i % 2 == 0) {
 			do {
-				a1 = CommonFunctionAndValues.getRandomInt(5, 17);
-				a2 = CommonFunctionAndValues.getRandomInt(5, 17);
-			} while (!(denom > a1 && denom > a2 && (a1+a2)<=denom && a1>a2));
+				denom = CommonFunctionAndValues.getRandomInt(3, 13);
+				a1 = CommonFunctionAndValues.getRandomInt(2, 10);
+				a2 = CommonFunctionAndValues.getRandomInt(2, 10);
+			} while (!(denom > a1 && denom > a2));
 			f1 = new Fraction(a1, denom);
 			f2 = new Fraction(a2, denom);
 			
 		} else  {
 			do {
-				denomLeft = CommonFunctionAndValues.getRandomInt(5, 34);;
-				denomRight = CommonFunctionAndValues.getRandomInt(5, 34);
-				a1 = CommonFunctionAndValues.getRandomInt(5, 17);
-				a2 = CommonFunctionAndValues.getRandomInt(5, 17);
+				denomLeft = CommonFunctionAndValues.getRandomInt(3, 13);;
+				denomRight = CommonFunctionAndValues.getRandomInt(3, 13);
+				a1 = CommonFunctionAndValues.getRandomInt(2, 13);
+				a2 = CommonFunctionAndValues.getRandomInt(2, 12);
 				gcdL = MathUtils.findGCD(a1,denomRight);
 				gcdR = MathUtils.findGCD(a2,denomLeft);
 			} while (denomLeft==denomRight
@@ -100,23 +101,10 @@ public class FractionMultiplication implements IQuestionFactory{
 	}
 	
 	private Set<String> buildChoices(Fraction f1,Fraction f2,Fraction result){
-		Fraction[] choices = new Fraction[6];
-		choices[0] = result;
-		boolean isMultiplication=false;
-		isMultiplication = (result.equals(f1.getResultWhenMultipliedBy(f2)));
-		if (isMultiplication){
-			choices[1] = result.inverse();
-			choices[2] = f1.getResultWhenMultipliedBy(f2.inverse());
-			choices[3] = f2.getResultWhenMultipliedBy(f1.inverse());
-			choices[4] = f1.inverse().getResultWhenMultipliedBy(f2.inverse());
-			choices[5] = new Fraction(f1.a * f2.a, f1.b);
-		} else {
-			choices[1] = result.inverse();
-			choices[2] = f1.getResultWhenDividedBy(f2.inverse());
-			choices[3] = f2.getResultWhenDividedBy(f1.inverse());
-			choices[4] = f1.inverse().getResultWhenDividedBy(f2.inverse());
-			choices[5] = new Fraction(f1.a * f2.a, f1.b );
-		}
+		Fraction[] choices = new Fraction[3];
+		choices[0] = result;		
+		choices[1] = result.inverse();
+		choices[2] = f1.getResultWhenMultipliedBy(f2.inverse());					
 		Set<String> choicesInString = new HashSet<String>();
 		for (Fraction f : choices) {
 			choicesInString.add(f.toString());
