5d8c20e889e8ce074ea6fea3806c8986ea099b12
Ewin Sutriandi <ewin.sutriandi@gmail.com>
2020-08-22 10:52:36 +0800

    change to multiple choice

--- QuestionGenerator/src/com/alza/quiz/qfactory/fraction/FractionRealWorldProblemScale.java
+++ QuestionGenerator/src/com/alza/quiz/qfactory/fraction/FractionRealWorldProblemScale.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
-
+import com.alza.quiz.model.MultipleChoiceQuiz;
 import com.alza.quiz.model.Quiz;
 import com.alza.quiz.model.QuizLevel;
-import com.alza.quiz.model.SimpleQuiz;
 import com.alza.quiz.qfactory.IQuestionFactory;
 import com.alza.quiz.util.CommonFunctionAndValues;
 
@@ -47,50 +46,54 @@ public class FractionRealWorldProblemScale implements IQuestionFactory{
 	public List<Quiz> generateQuizList() {
 		List<Quiz> ql = new ArrayList<Quiz>();
 		for (int i=0;i<numq;i++){
-			SimpleQuiz q = new SimpleQuiz();
-			
-			q.setLessonClassifier(bundle.getString("mathelementary"));
-			q.setLessonGrade(5);
-			q.setLessonCategory(bundle.getString("fraction"));
-			q.setLessonSubcategory(bundle.getString("fraction.findactfromscale"));
-			q.setDifficultyLevel(QuizLevel.SEDANG);
-			
-			int rnd = CommonFunctionAndValues.getRandomInt(0, sces.size());
-			String sce = getRandomScenario(rnd);
-			String param =  getParams(rnd);
-			// prepare lower and upper bound 0020082070, 002 lobo 008 hibo, 20 low pct, hi pct
-			int loVal, hiVal, multip, scale;
-			double loLen, hiLen, modLen, reaLen;
-			int corr;
-			loVal = Integer.parseInt(param.substring(0, 6));
-			hiVal = Integer.parseInt(param.substring(6, 12));
-			loLen = Integer.parseInt(param.substring(12, 14));
-			hiLen = Integer.parseInt(param.substring(14, 16));
-			corr = Integer.parseInt(param.substring(16, 22)); // correction caused by conversion in unit of measure
-			
-			do {
-				multip = CommonFunctionAndValues.getRandomInt(2,  (hiVal/loVal)+1);
-				modLen = CommonFunctionAndValues.getRandomInt((int)loLen, (int) (hiLen+1));
-			} while (false); 
-			scale = multip * loVal;
-			reaLen = modLen * scale / corr;
-			String corrAns = CommonFunctionAndValues.DF2PLC.format(reaLen);
-			sce = sce.replace("#modlength?", String.valueOf(modLen));
-			sce = sce.replace("#scale?", String.valueOf(scale));
-			sce = CommonFunctionAndValues.buildScenario(sce);
-			q.setQuestion(sce);
-			q.setCorrectAnswer(corrAns);
-			/**
-			q.setChoices(corrAns,
-					CommonFunctionAndValues.DF2PLC.format(reaLen/10),
-					CommonFunctionAndValues.DF2PLC.format(reaLen*10),
-					CommonFunctionAndValues.DF2PLC.format(reaLen*multip),
-					CommonFunctionAndValues.DF2PLC.format(reaLen/multip));
-					*/
-			q.setLocale(loc);
+			MultipleChoiceQuiz q; 
+			q = createSingleQuiz();			
+			setSecondaryAttributes(q);
 			ql.add(q);
 		}	
 		return ql;
+	}
+	private MultipleChoiceQuiz createSingleQuiz() {
+		double[] multips = {0.1,10,5,20,100,0.01};
+		MultipleChoiceQuiz q;
+		q = new MultipleChoiceQuiz();					
+		int rnd = CommonFunctionAndValues.getRandomInt(0, sces.size());
+		String sce = getRandomScenario(rnd);
+		String param =  getParams(rnd);
+		// prepare lower and upper bound 0020082070, 002 lobo 008 hibo, 20 low pct, hi pct
+		int loVal, hiVal, multip, scale;
+		double loLen, hiLen, modLen, reaLen;
+		int corr;
+		loVal = Integer.parseInt(param.substring(0, 6));
+		hiVal = Integer.parseInt(param.substring(6, 12));
+		loLen = Integer.parseInt(param.substring(12, 14));
+		hiLen = Integer.parseInt(param.substring(14, 16));
+		corr = Integer.parseInt(param.substring(16, 22)); // correction caused by conversion in unit of measure		
+		do {
+			multip = CommonFunctionAndValues.getRandomInt(2,  (hiVal/loVal)+1);
+			modLen = CommonFunctionAndValues.getRandomInt((int)loLen, (int) (hiLen+1));
+		} while (false); 
+		scale = multip * loVal;
+		reaLen = modLen * scale / corr;
+		String corrAns = CommonFunctionAndValues.DF2PLC.format(reaLen);
+		sce = sce.replace("#modlength?", String.valueOf(modLen));
+		sce = sce.replace("#scale?", String.valueOf(scale));
+		sce = CommonFunctionAndValues.buildScenario(sce);
+		q.setQuestion(sce);
+		q.setCorrectAnswer(corrAns);		
+		q.setChoices(corrAns,
+				CommonFunctionAndValues.DF2PLC.format(reaLen * CommonFunctionAndValues.getRandom(multips)),
+				CommonFunctionAndValues.DF2PLC.format(reaLen * CommonFunctionAndValues.getRandom(multips))
+				);		
+		return q;
+	}
+	private void setSecondaryAttributes(Quiz q) {
+		q.setLocale(loc);
+		q.setLessonClassifier(bundle.getString("mathelementary"));
+		q.setLessonGrade(5);
+		q.setLessonCategory(bundle.getString("fraction"));
+		q.setLessonSubcategory(bundle.getString("fraction.findactfromscale"));
+		q.setDifficultyLevel(QuizLevel.SEDANG);
 	}
 
 	private String getParams(int rnd) {
