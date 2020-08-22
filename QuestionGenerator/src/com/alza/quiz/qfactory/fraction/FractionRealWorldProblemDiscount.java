fb9a0bdf7d548ce885c34ce51f22fda9ea1a4d9a
Ewin Sutriandi <ewin.sutriandi@gmail.com>
2020-08-22 10:58:10 +0800

    reduce choices size

--- QuestionGenerator/src/com/alza/quiz/qfactory/fraction/FractionRealWorldProblemDiscount.java
+++ QuestionGenerator/src/com/alza/quiz/qfactory/fraction/FractionRealWorldProblemDiscount.java
@@ -48,52 +48,58 @@ public class FractionRealWorldProblemDiscount implements IQuestionFactory{
 	public List<Quiz> generateQuizList() {
 		List<Quiz> ql = new ArrayList<Quiz>();
 		for (int i=0;i<numq;i++){
-			MultipleChoiceQuiz q = new MultipleChoiceQuiz();
-			
-			q.setLessonClassifier(bundle.getString("mathelementary"));
-			q.setLessonGrade(5);
-			q.setLessonCategory(bundle.getString("fraction"));
-			q.setLessonSubcategory(bundle.getString("fraction.finddiscprice"));
-			q.setDifficultyLevel(QuizLevel.SEDANG);
-			
-						
-			int rnd = CommonFunctionAndValues.getRandomInt(0, sces.size());
-			String sce = getRandomScenario(rnd);
-			String param =  getParams(rnd);
-			// prepare lower and upper bound 0020082070, 002 lobo 008 hibo, 20 low pct, hi pct
-			int loPrice, hiPrice;
-			int loDisc, hiDisc;
-			loPrice = Integer.parseInt(param.substring(0, 3));
-			hiPrice = Integer.parseInt(param.substring(3, 6));
-			loDisc = Integer.parseInt(param.substring(6, 8));
-			hiDisc = Integer.parseInt(param.substring(8, 10));
-			//calculate ratio r1 : r2
-			double oriprice, disc;
-			do {
-				oriprice = CommonFunctionAndValues.getRandomInt(loPrice, hiPrice+1);
-				disc = CommonFunctionAndValues.getRandomInt(loDisc, hiDisc+1);
-			} while (false); 
-			double deltaPrice = oriprice * (double) disc / 100;
-			double newPrice = oriprice - deltaPrice;
-			double wrongPrice = oriprice + deltaPrice;
-			double wrongval2 = oriprice - (deltaPrice/2);
-			double wrongval3 = oriprice - (deltaPrice/3);
-			String discPct = new Fraction((int)disc,100).getPercentageNoDecimal();
-			String corrAns = CommonFunctionAndValues.DF2PLC.format(newPrice);
-			sce = sce.replace("#oriprice?", String.valueOf(oriprice));
-			sce = sce.replace("#pctdisc?", String.valueOf(discPct));
-			sce = CommonFunctionAndValues.buildScenario(sce);
-			q.setQuestion(sce);
-			q.setCorrectAnswer(corrAns);
-			q.setChoices(corrAns,
-					CommonFunctionAndValues.DF2PLC.format(wrongval3),
-					CommonFunctionAndValues.DF2PLC.format(wrongPrice),
-					CommonFunctionAndValues.DF2PLC.format(wrongval2),
-					CommonFunctionAndValues.DF2PLC.format(deltaPrice));
-			q.setLocale(loc);
+			MultipleChoiceQuiz q; 
+			q = createSingleQuiz();
+			setSecondaryAttributes(q);
 			ql.add(q);
 		}	
 		return ql;
+	}
+	private MultipleChoiceQuiz createSingleQuiz() {
+		MultipleChoiceQuiz q;
+		q = new MultipleChoiceQuiz();											
+		int rnd = CommonFunctionAndValues.getRandomInt(0, sces.size());
+		String sce = getRandomScenario(rnd);
+		String param =  getParams(rnd);
+		// prepare lower and upper bound 0020082070, 002 lobo 008 hibo, 20 low pct, hi pct
+		int loPrice, hiPrice;
+		int loDisc, hiDisc;
+		loPrice = Integer.parseInt(param.substring(0, 3));
+		hiPrice = Integer.parseInt(param.substring(3, 6));
+		loDisc = Integer.parseInt(param.substring(6, 8));
+		hiDisc = Integer.parseInt(param.substring(8, 10));
+		//calculate ratio r1 : r2
+		double oriprice, disc;
+		do {
+			oriprice = CommonFunctionAndValues.getRandomInt(loPrice, hiPrice+1);
+			disc = CommonFunctionAndValues.getRandomInt(loDisc, hiDisc+1);
+		} while (false); 
+		double deltaPrice = oriprice * (double) disc / 100;
+		double newPrice = oriprice - deltaPrice;
+		double wrongPrice = oriprice + deltaPrice;
+		double wrongval2 = oriprice - (deltaPrice/2);
+		double wrongval3 = oriprice - (deltaPrice/3);
+		String discPct = new Fraction((int)disc,100).getPercentageNoDecimal();
+		String corrAns = CommonFunctionAndValues.DF2PLC.format(newPrice);
+		sce = sce.replace("#oriprice?", String.valueOf(oriprice));
+		sce = sce.replace("#pctdisc?", String.valueOf(discPct));
+		sce = CommonFunctionAndValues.buildScenario(sce);
+		q.setQuestion(sce);
+		q.setCorrectAnswer(corrAns);
+		double[] wrongAns = {wrongval3,wrongPrice,wrongval2,deltaPrice};
+		q.setChoices(corrAns,
+				CommonFunctionAndValues.DF2PLC.format(CommonFunctionAndValues.getRandom(wrongAns)),
+				CommonFunctionAndValues.DF2PLC.format(CommonFunctionAndValues.getRandom(wrongAns))
+			);				
+		return q;
+	}
+	private void setSecondaryAttributes(MultipleChoiceQuiz q) {
+		q.setLessonClassifier(bundle.getString("mathelementary"));
+		q.setLessonGrade(5);
+		q.setLessonCategory(bundle.getString("fraction"));
+		q.setLessonSubcategory(bundle.getString("fraction.finddiscprice"));
+		q.setDifficultyLevel(QuizLevel.SEDANG);
+		q.setLocale(loc);
 	}
 
 	private String getParams(int rnd) {
