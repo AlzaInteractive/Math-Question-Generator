6da35cc9fb7144125a489ad4390728c38b022667
Ewin Sutriandi <ewin.sutriandi@gmail.com>
2020-08-22 10:11:57 +0800

    refactor, reduce choices size

--- QuestionGenerator/src/com/alza/quiz/qfactory/fraction/FractionRealWorldProblemFindFromPercentage.java
+++ QuestionGenerator/src/com/alza/quiz/qfactory/fraction/FractionRealWorldProblemFindFromPercentage.java
@@ -48,43 +48,49 @@ public class FractionRealWorldProblemFindFromPercentage implements IQuestionFact
 	public List<Quiz> generateQuizList() {
 		List<Quiz> ql = new ArrayList<Quiz>();
 		for (int i=0;i<numq;i++){
-			MultipleChoiceQuiz q = new MultipleChoiceQuiz();
-			q.setDifficultyLevel(QuizLevel.SEDANG);
-			q.setLessonSubcategory(bundle.getString("fraction.findvalifpctandsumknown"));
-			int rnd = CommonFunctionAndValues.getRandomInt(0, sces.size());
-			String sce = getRandomScenario(rnd);
-			String param =  getParams(rnd);
-			Fraction rat1;
-			// prepare lower and upper bound 0020082070, 002 lobo 008 hibo, 20 low pct, hi pct
-			double loT, hiT;
-			int loPct, hiPct;
-			loT = Integer.parseInt(param.substring(0, 3));
-			hiT = Integer.parseInt(param.substring(3, 6));
-			loPct = Integer.parseInt(param.substring(6, 8));
-			hiPct = Integer.parseInt(param.substring(8, 10));
-			//calculate total
-			int total, tot1, tot2;
-			total = CommonFunctionAndValues.getRandomInt((int)loT,(int)hiT);
-			//calculate total for item 1, ensure it came between pct bound
-			do {
-				tot1 = CommonFunctionAndValues.getRandomInt(0, total);
-				rat1 = new Fraction(tot1, total);  
-			} while (!rat1.isBetween(new Fraction(loPct, 100), new Fraction(hiPct, 100))); 
-			tot2 = total - tot1;
-			String corrAns = String.valueOf(tot2);
-			Fraction f = new Fraction(tot1, total);
-			String pct = f.getPercentageNoDecimal();
-			sce = sce.replace("#total?", String.valueOf(total));
-			sce = sce.replace("#pct1?", pct);
-			sce = CommonFunctionAndValues.buildScenario(sce);
-			q.setQuestion(sce);
-			q.setCorrectAnswer(String.valueOf(corrAns));
-			q.setChoices(total,tot2,tot1,Math.abs(tot1-tot2));
-			q.setLessonGrade(5);
-			q.setLocale(loc);
+			MultipleChoiceQuiz q = new MultipleChoiceQuiz();			
+			createSingleQuestion(q);
+			setSecondaryAttributes(q);
 			ql.add(q);
 		}	
 		return ql;
+	}
+	private void createSingleQuestion(MultipleChoiceQuiz q) {
+		int rnd = CommonFunctionAndValues.getRandomInt(0, sces.size());
+		String sce = getRandomScenario(rnd);
+		String param =  getParams(rnd);
+		Fraction rat1;
+		// prepare lower and upper bound 0020082070, 002 lobo 008 hibo, 20 low pct, hi pct
+		double loT, hiT;
+		int loPct, hiPct;
+		loT = Integer.parseInt(param.substring(0, 3));
+		hiT = Integer.parseInt(param.substring(3, 6));
+		loPct = Integer.parseInt(param.substring(6, 8));
+		hiPct = Integer.parseInt(param.substring(8, 10));
+		//calculate total
+		int total, tot1, tot2;
+		total = CommonFunctionAndValues.getRandomInt((int)loT,(int)hiT);
+		//calculate total for item 1, ensure it came between pct bound
+		do {
+			tot1 = CommonFunctionAndValues.getRandomInt(0, total);
+			rat1 = new Fraction(tot1, total);  
+		} while (!rat1.isBetween(new Fraction(loPct, 100), new Fraction(hiPct, 100))); 
+		tot2 = total - tot1;
+		String corrAns = String.valueOf(tot2);
+		Fraction f = new Fraction(tot1, total);
+		String pct = f.getPercentageNoDecimal();
+		sce = sce.replace("#total?", String.valueOf(total));
+		sce = sce.replace("#pct1?", pct);
+		sce = CommonFunctionAndValues.buildScenario(sce);
+		q.setQuestion(sce);
+		q.setCorrectAnswer(String.valueOf(corrAns));
+		q.setChoices(tot2,tot1,Math.abs(tot1-tot2));
+	}
+	private void setSecondaryAttributes(MultipleChoiceQuiz q) {
+		q.setDifficultyLevel(QuizLevel.SEDANG);
+		q.setLessonSubcategory(bundle.getString("fraction.findvalifpctandsumknown"));
+		q.setLessonGrade(5);
+		q.setLocale(loc);
 	}
 
 	private String getParams(int rnd) {
