1e4969693b9ff94b9243690d4824d9fbeb506dec
Ewin Sutriandi <ewin.sutriandi@gmail.com>
2020-12-19 08:09:49 +0800

    level 5

---
+++ QuestionGenerator/src/com/alza/quiz/qfactory/algebra/Level5SimpleQuadraticPlain.java
@@ -0,0 +1,160 @@
+package com.alza.quiz.qfactory.algebra;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Random;
+import java.util.ResourceBundle;
+import java.util.concurrent.ThreadLocalRandom;
+
+import com.alza.quiz.model.ISingleQuizPrimaryAttributeGenerator;
+import com.alza.quiz.model.MultipleChoiceQuiz;
+import com.alza.quiz.model.Quiz;
+import com.alza.quiz.model.QuizLevel;
+import com.alza.quiz.qfactory.IQuestionFactory;
+import com.alza.quiz.util.CommonFunctionAndValues;
+
+public class Level5SimpleQuadraticPlain implements IQuestionFactory{
+
+	private int numOfQuestion = 5;
+	private Map<Integer, ProblemSkeleton> qMap = new HashMap<Integer, Level5SimpleQuadraticPlain.ProblemSkeleton>();
+	private Locale loc;
+	private ResourceBundle bundle;
+	private ResourceBundle bundleAlgebra;
+	
+	public Level5SimpleQuadraticPlain(Locale loc){
+		this.loc = loc;
+		initStringFromLocale();
+	}
+	public Level5SimpleQuadraticPlain(){
+		this.loc = new Locale("in", "ID");
+		initStringFromLocale();
+	}
+	private void initStringFromLocale(){
+		bundle = ResourceBundle.getBundle("lang.langbundle", loc);
+		bundleAlgebra = ResourceBundle.getBundle("lang.langbundle-algebra", loc);
+		
+	}
+
+	@Override
+	public Quiz generateQuiz() {
+		List<Quiz> quizList = generateQuizList();
+		int rnd = new Random().nextInt(quizList.size()); 
+		return quizList.get(rnd);
+	}
+
+	@Override
+	public Quiz generateQuiz(QuizLevel quizLevel) {
+		return generateQuiz();
+	}
+
+	@Override
+	public List<Quiz> generateQuizList() {
+		List<Quiz> lq = new ArrayList<Quiz>();
+		for (int i=0;i<this.numOfQuestion;i++) {
+			ProblemSkeleton p = generateUniqueProblem(i);
+			Quiz q = p.generateSingleQuiz();
+			setQuizSecondaryAttributes(q);
+			lq.add(q);
+		}
+		return lq;
+	}
+			
+	private ProblemSkeleton generateUniqueProblem(int idx) {		
+		ProblemSkeleton p;
+		do {			
+			p = new ProblemSkeleton(idx);
+		} while (qMap.containsKey(p.hash()));
+		qMap.put(p.hash(), p);
+		return p;
+	}
+
+	@Override
+	public List<Quiz> generateQuizList(int numOfQuestion) {
+		this.numOfQuestion  = numOfQuestion;
+		return generateQuizList();
+	}
+			
+	private void setQuizSecondaryAttributes(Quiz q) {
+		q.setDifficultyLevel(QuizLevel.MUDAH);
+		q.setLessonSubcategory(bundleAlgebra.getString("algebra.level5.quadratic"));
+		q.setLessonClassifier(bundle.getString("mathelementary"));
+		q.setLessonGrade(5);
+		q.setSubCategoryOrder(6);		
+		q.setLessonCategory(bundle.getString("algebra"));
+		q.setLocale(loc);
+	}
+	
+	protected class ProblemSkeleton implements ISingleQuizPrimaryAttributeGenerator{
+		int a;		
+		String var;		
+		final String[] VARSYM = {"x","y","z"};
+		ProblemSkeleton(int idx) {
+			var = VARSYM[ThreadLocalRandom.current().nextInt(0, VARSYM.length)];						
+			a = ThreadLocalRandom.current().nextInt(3, 9);									
+		}
+		
+		int hash() {
+			String s = a+" ";
+			return (CommonFunctionAndValues.hashSimple(s));
+		}		
+		
+		
+		private String replaceAllSymbols(String s) {
+			s = s.replace("v1", String.valueOf(this.a*this.a));		
+			s = s.replace("VAR", String.valueOf(var));
+			return s;
+		}
+
+		@Override
+		public String generateQuestion() {
+			String s = "VAR^2 = v1";			
+			s = replaceAllSymbols(s);
+			return s;
+		}
+
+		@Override
+		public String generateQuestionMathjax() {
+			String s = generateQuestion();
+			s = CommonFunctionAndValues.enclosedWithMathJaxExp(s);
+			String s2 = var +"=?";
+			s2 = CommonFunctionAndValues.enclosedWithMathJaxExp(s2);
+			s = s +" "+ s2;
+			return s;
+		}
+
+		@Override
+		public String generateQuestionWolfram() {	
+			return generateQuestion();
+		}
+
+		@Override
+		public String[] generateChoices() {
+				
+			String[] choices = {this.a+",-"+this.a,
+					(this.a*2)+",-"+(this.a*2),
+					this.a+""};
+			
+			return choices;
+		}
+
+		@Override
+		public String generateAnswer() {			
+			return String.valueOf(this.a+",-"+this.a);
+		}
+
+		@Override
+		public Quiz generateSingleQuiz() {
+			MultipleChoiceQuiz q = new MultipleChoiceQuiz();
+			q.setQuestion(generateQuestionMathjax());
+			q.setProblemString(generateQuestionWolfram());
+			q.setCorrectAnswer(generateAnswer());
+			q.setChoices(generateChoices());			
+			return q;
+		}		
+		
+	}
+
+}
